d4b0eed20154580550d0c9d2361708a47d8e64a6
unknown <patricio.ramirez.c@gmail.com>
2022-08-04 12:07:37 -0400

    logicIsWorking

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,78 +1,58 @@
 const list = document.querySelector("#taskList");
-      const newTask = document.querySelector("#iNewTask");
-      const btnAdd = document.querySelector("#btnAdd");
-      const total = document.querySelector("#total");
-      const done = document.querySelector("#done");
-      const iCheck = document.querySelectorAll("#check");
+const newTask = document.querySelector("#iNewTask");
+const btnAdd = document.querySelector("#btnAdd");
+const total = document.querySelector("#total");
+const done = document.querySelector("#done");
+const iCheck = document.querySelector("input[type=checkbox]");
 
-      const tasks = [];
+const tasks = [
+  { id: 1, task: "tarea1", chck: false },
+  { id: 2, task: "tarea2", chck: false },
+  { id: 3, task: "tarea3", chck: false },
+];
 
-      btnAdd.addEventListener("click", () => {
-        //Validation
-        if (newTask.value !== "") {
-          tasks.push({ id: Date.now(), task: newTask.value, chck: false });
-        } else {
-          alert("No puedes dejar la tarea vacía");
-        }
+btnAdd.addEventListener("click", () => {
+  //Validation
+  if (newTask.value !== "") {
+    tasks.push({ id: Date.now(), task: newTask.value, chck: false });
+  } else {
+    alert("No puedes dejar la tarea vacía");
+  }
 
-        newTask.value = "";
-        updateTasks();
-      });
-      
-      deleteTask = (id) => {
-        tasks.splice(
-          tasks.findIndex((task) => task.id === id),
-          1
-        );
-        updateTasks();
-      };
+  newTask.value = "";
+  updateTasks();
+});
 
-      updateTasks = () => {
-        let html = "";
-        tasks.forEach(({ id, task, chck}) => {
-          html += `<li class="list">${id} ${task} <input id="check${id}" type="checkbox" />
-            <button id="bDelete${id}" onclick="deleteTask(${tasks.id})" >Borrar tarea</button></li>`;
-        });
+deleteTask = (id) => {
+  tasks.splice(
+    tasks.findIndex((task) => task.id == id),
+    1
+  );
+  updateTasks();
+};
 
-      
-        const check = () =>
-        done.innerHTML = iCheck.filter((task) => task.checked === true).length;
+updateCheckValue = (id) => {
+  const cId = tasks.find((task) => task.id == id);
+  cId.chck = !cId.chck;
+  console.log("visualizando chck", cId.chck);
 
+  updateTasks();
+};
 
+updateTasks = () => {
+  let html = "";
+  tasks.forEach(({ id, task, chck }) => {
+    html += `<li class="list">${id} ${task} <input id="check${id}" ${
+      chck ? "checked" : ""
+    } type="checkbox" onChange="updateCheckValue(${id});" />
+            <button id="bDelete${id}" onclick="deleteTask(${id})" >Borrar tarea</button></li>`;
+  });
+  check();
+  total.innerHTML = tasks.length;
+  list.innerHTML = html;
+};
 
-        //checkbox checked
-        // const iCheck = document.querySelectorAll("input[type=checkbox]");
+const check = () =>
+  (done.innerHTML = tasks.filter((task) => task.chck === true).length);
 
-        // const checks = () =>
-        //   done.innerHTML = iCheck.filter((i) => i.checked === true).length;
-
-        
-          // done.innerHTML = iCheck.filter(checkbox =>
-          //   checkbox.checked === true) ;
-
-        // const checks = () => {
-        //   iCheck.forEach((checkbox) => {
-        //     if (checkbox.checked === true) {
-        //       done.innerHTML = Number(done.innerHTML) + 1;
-        //     }
-        //   });
-        // };
-
-        
-        // iCheck.forEach((checkbox) => {
-
-        //   checkbox.addEventListener("click", () => {
-        //     /* checkbox.checked ? done++ ; */
-        //       if (checkbox.checked = true) {
-        //       done.innerHTML =  Number(done.innerHTML) +1;
-        //     }
-        //   });
-        // });
-
-        console.log(html);
-        console.log(tasks);
-        console.log(done.length );
-        console.log("Visualizando checks:", done);
-        total.innerHTML = tasks.length;
-        list.innerHTML = html;
-      };
+updateTasks();
